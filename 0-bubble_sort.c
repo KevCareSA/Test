e81b7bdf2b98fd29c9481102bb0b71c69981986b
BradleyGilden <bradleygilden@gmail.com>
2023-07-20 13:25:15 +0200

    more algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,22 +8,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp, sorted = 1;
+	size_t i, j;
+	int temp, sorted = 1;
 
-    for (i = 0; i < size - 1 && sorted; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                sorted = 1;
-                print_array(array, size);
-                
-            }
-        }
-    }
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1 && sorted; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				sorted = 1;
+				print_array(array, size);
+				
+			}
+		}
+	}
 }
---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function implements bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index = 0;
+	int temp, min;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		for (j = i; j < size - 1; j++)
+		{
+			if (array[j + 1] < min)
+			{
+				min = array[j + 1];
+				min_index = j + 1;
+			}
+		}
+		if (min != array[i])
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
+		}
+	}
+}
