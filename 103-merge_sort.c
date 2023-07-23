9782d3fa6dd647714c86e30b1c965abacd637dee
BradleyGilden <bradleygilden@gmail.com>
2023-07-23 21:31:13 +0200

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,19 +1,62 @@
 #include "sort.h"
 
-void merge(int *arr, int *copy, int start, int middle, int end);
+void merge(int *aux, int *arr, int start, int middle, int end)
+{
+	int k, i = start, j = middle;
 
-void topdown_merge_sort(int *arr, int *copy, int start, int end)
+	printf("Merging...\n");
+	printf("[left]: ");
+	for (k = start; k < middle; k++)
+	{
+		if (k + 1 == middle)
+			printf("%d\n", arr[k]);
+		else
+			printf("%d ", arr[k]);
+	}
+	printf("[right]: ");
+	for (k = middle; k < end; k++)
+	{
+		if (k + 1 == end)
+			printf("%d\n", arr[k]);
+		else
+			printf("%d ", arr[k]);
+	}
+	for (k = start; k < end; k++)
+	{
+		if (i < middle && (j >= end || arr[i] <= arr[j]))
+		{
+			aux[k] = arr[i];
+			i = i + 1;
+		}
+		else
+		{
+			aux[k] = arr[j];
+			j = j + 1;
+		}
+	}
+	printf("[Done]: ");
+	for (k = start; k < end; k++)
+	{
+		if (k + 1 == end)
+			printf("%d\n", arr[k]);
+		else
+			printf("%d ", arr[k]);
+	}
+}
+
+void topdown_merge_sort(int *aux, int *arr, int start, int end)
 {
 	int middle;
 
-	if((end - start) <= 1)
+	if ((end - start) <= 1)
 		return;
 
-	middle = (end - start) / 2;
+	middle = (end + start) / 2;
 
-	topdown_merge_sort(arr, copy, start, middle);
-	topdown_merge_sort(arr, copy, middle, end);
-    merge(arr, copy, start, middle, end);
+	topdown_merge_sort(arr, aux, start, middle);
+	topdown_merge_sort(arr, aux, middle, end);
+
+	merge(aux, arr, start, middle, end);
 }
 
 void merge_sort(int *array, size_t size)
