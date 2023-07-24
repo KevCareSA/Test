d34453c4687f0dcfd2c7bb114ae3d503c7b5bc80
BradleyGilden <bradleygilden@gmail.com>
2023-07-24 19:14:56 +0200

    radix sort

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -15,8 +15,8 @@ int max_digit_count(int *arr, size_t size)
 	size_t i;
 	int max, count;
 
-	max = count_digits(arr[0]);
-	for(i = 1; i < size; i++)
+	max = 1;
+	for(i = 0; i < size; i++)
 	{
 		count = count_digits(arr[i]);
 		max = count > max ? count : max;
@@ -35,9 +35,57 @@ int p_value(int value, int count)
 	return (pval);
 }
 
+/**
+ * count_arr - finds the maximum integer and initializes counting array
+               of size (k + 1) with 0 for each element, where k is the
+               maximum integer of {array}
+ * @array: array to sort
+ * @size: size of the array
+ * @max: maximum integer in {array}
+ * @copy: copy of {array}
+ * Return: returns pointer to counting array
+ */
+void radix_count_arr(int *arr, int *copy, int *ca, size_t size, int pval)
+{
+	size_t i;
+
+	/*initializing counting array*/
+	for (i = 0; i < 10; i++)
+		ca[i] = 0;
+	/*copying contents of original array to the copy*/
+	for (i = 0; i < size; i++)
+		copy[i] = arr[i];
+	/*counting value occurences in counting array*/
+	for (i = 0; i < size; i++)
+		ca[p_value(arr[i], pval)] += 1;
+	/*cumulative count on the counting array*/
+	for (i = 1; i < 10; i++)
+		ca[i] += ca[i - 1];
+
+	for (i = 0; i < size; i++)
+	{
+		arr[ca[p_value(copy[i], pval)] - 1] = copy[i];
+		ca[p_value(copy[i], pval)] -= 1;
+	}
+}
+
 void radix_sort(int *array, size_t size)
 {
-	int count, *copy = NULL;
+	int i, count, *copy = NULL, carray[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	if (array == NULL || size < 2)
+		return;
+
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		return;
 
 	count = max_digit_count(array, size);
+
+	for(i = 0; i < count; i++)
+	{
+		radix_count_arr(array, copy, carray, size, i);
+		print_array(array, size);
+	}
+	free(copy);
 }
