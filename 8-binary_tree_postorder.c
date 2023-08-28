8bbf6b9d3cc5c9113322e85e66090ac52ab0641d
BradleyGilden <bradleygilden@gmail.com>
2023-08-28 22:03:15 +0200

    update

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - applies function (func) on node data while
+ *                        traversing tree in-orderly
+ * @tree: pointer to the root node
+ * @func: function pointer
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL && func != NULL)
+	{
+        binary_tree_postorder(tree->left, func);
+        binary_tree_postorder(tree->right, func);
+        func(tree->n);
+    }
+}
