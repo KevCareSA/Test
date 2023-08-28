5924696fffa891a8d2d6782e7ca0dbc95c42c83d
BradleyGilden <bradleygilden@gmail.com>
2023-08-28 19:04:02 +0200

    is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,12 @@
 #include "binary_trees.h"
+
+/**
+ * binary_tree_delete - frees binary tree from heap using post-order traversal
+ * @tree: pointer to the root node
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+    if (node == NULL || node->left != NULL || node->right != NULL)
+        return (0);
+    return (1);
+}
