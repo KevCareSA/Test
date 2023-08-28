e601e8120640179573f5fa1864547fa324ba2da7
BradleyGilden <bradleygilden@gmail.com>
2023-08-28 19:38:22 +0200

    comment update

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,10 +3,11 @@
 /**
  * binary_tree_is_root - checks if node is the root node
  * @node: pointer to a potential root node
+ * Return: 1 if root 0 otherwise
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL || node->parent != NULL)
-        return (0);
-    return (1);
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
 }
