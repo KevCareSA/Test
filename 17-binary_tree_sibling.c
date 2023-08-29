ced0a0893d7c045f3987cbac8a2e9874859bec97
BradleyGilden <bradleygilden@gmail.com>
2023-08-29 18:00:39 +0200

    sibling detection

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * full_check - determine if a tree is perfect or not.
- * @tree: root node of a tree
- * Return: 1 if a perfect tree,  0 otherwise
+ * binary_tree_sibling - finds address of sibling node
+ * @node: node to check
+ * Return: address of sibling node, null otherwise
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (node == NULL || node->parent == NULL)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    if (node->parent->left == NULL || node->parent->right == NULL)
-        return (NULL);
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
 
-    if (node->parent->left == node)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
