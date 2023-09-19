204431ed5664a034b3d4346511bb0244c0d1470b
BradleyGilden <bradleygilden@gmail.com>
2023-09-19 13:20:56 +0200

    update amenity and place

--- models/place.py
+++ models/place.py
@@ -38,7 +38,7 @@ class Place(BaseModel, Base):
         reviews = relationship("Review", backref="place",
                                cascade="all, delete-orphan")
         amenities = relationship("Amenity", secondary="place_amenity",
-                                 viewonly=False)
+                                 viewonly=False, overlaps='place_amenities')
         amenity_ids = []
     else:
         city_id = ""
